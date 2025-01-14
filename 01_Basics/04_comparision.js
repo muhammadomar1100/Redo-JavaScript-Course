66c306d9944d8da980c7003411cc0f0994dc351a
TheCoderz <89317339+muhammadomar1100@users.noreply.github.com>
2025-01-14 12:14:04 +0000

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1); // greater than
+// console.log(2 < 1); // lesser than
+// console.log(2 >= 1); // greater than or equal
+// console.log(2 == 1); // equal 
+// console.log(2 != 1); // not equal
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
