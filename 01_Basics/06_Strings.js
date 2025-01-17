b1b92094ffc348c490e254d37a0bf686e9d1c726
TheCoderz <89317339+muhammadomar1100@users.noreply.github.com>
2025-01-17 18:12:58 +0000

    06_Strings

---
+++ 01_Basics/06_Strings.js
@@ -0,0 +1,24 @@
+const name = "Omar"
+const repoCount = "10 " 
+const age = 17
+
+// DON'T WRITE IT LIKE THIS ITS OUTDATED 
+// console.log(name + repoCount + age)
+
+// Instead write like this
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount} and I am ${age} old`);
+
+const gameName = new String('Omar');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// to find the length of the word do
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+// POSITION OF THE number 
+console.log(gameName.charAt('2'));
+// POSITION OF THE CHARACTER
+console.log(gameName.indexOf('t'));
+
