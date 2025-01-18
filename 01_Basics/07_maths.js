0fa6fa60055865ba44f4737524f188836aba588b
TheCoderz <89317339+muhammadomar1100@users.noreply.github.com>
2025-01-18 17:05:17 +0000

    07_maths

---
+++ 01_Basics/07_maths.js
@@ -0,0 +1,14 @@
+const score = 900
+
+const balance = new Number(100)
+console.log(balance);
+
+// to change a certain part of a url
+
+const url = "https://muhammadomar.com/omar%40muhammad"
+
+console.log(url.replace('%40', "-"))
+
+// to find if a url consists of something like a letter do
+
+console.log(url.includes('omar'))
