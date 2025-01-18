62397af87be83ee021f71e14193c6719c2ef6525
TheCoderz <89317339+muhammadomar1100@users.noreply.github.com>
2025-01-18 17:05:38 +0000

    06_Strings

--- 01_Basics/06_Strings.js
+++ 01_Basics/06_Strings.js
@@ -22,3 +22,12 @@ console.log(gameName.charAt('2'));
 // POSITION OF THE CHARACTER
 console.log(gameName.indexOf('t'));
 
+// to change a certain part of a url
+
+const url = "https://muhammadomar.com/omar%40muhammad"
+
+console.log(url.replace('%40', "-"))
+
+// to find if a url consists of something like a letter do
+
+console.log(url.includes('omar'))
